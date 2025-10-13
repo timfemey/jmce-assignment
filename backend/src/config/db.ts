a9c1141568de9751f772f8129d9000deec38db67
timfemey <isholaobafemi@gmail.com>
2025-10-13 15:23:50 +0100

    entered public connection String

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
+  connectionString:
+    process.env.DB_USER ||
+    "postgresql://femi:6eGe0NbCl2i1cBw4McWM3Md2TmZlWVhw@dpg-d3mgire3jp1c73fuun70-a/jmce", //Wont work unless running on the cloud with my particular account
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || "5432"),
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
 });
 
 export default pool;
