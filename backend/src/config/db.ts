103def04ff9f6682e92082c540e13d90a16a5e1a
timfemey <isholaobafemi@gmail.com>
2025-10-13 15:24:52 +0100

    if no local user then use connectionString

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString:
-    process.env.DB_USER ||
-    "postgresql://femi:6eGe0NbCl2i1cBw4McWM3Md2TmZlWVhw@dpg-d3mgire3jp1c73fuun70-a/jmce", //Wont work unless running on the cloud with my particular account
+  connectionString: process.env.DB_USER
+    ? undefined
+    : "postgresql://femi:6eGe0NbCl2i1cBw4McWM3Md2TmZlWVhw@dpg-d3mgire3jp1c73fuun70-a/jmce", //Wont work unless running on the cloud with my particular account
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
